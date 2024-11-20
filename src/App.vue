d9c4dd6cfe1bd6993d1f646db0c011c021854e5b
Maurice Duchmann <mauriceduchmann@MacBook-Pro-von-Maurice.local>
2024-11-20 14:59:25 +0100

    Beispielcode entfernt

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <StatusHeader />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import StatusHeader from './components/StatusHeader.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    StatusHeader,
   },
 });
 </script>
---
+++ src/components/StatusHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>HelloWorld</h1>
+  <head class="status-header">
+    <div class="status-item">
+        <span class="ststus-label">PHOTO-BOX:</span>
+    </div>
+    <div class="status-item">
+        <span class="ststus-label">STROM-BOX:</span>
+    </div>
+    <div class="status-item">
+        <span class="ststus-label">TEMPERATUR:</span>
+    </div>
+    <div class="status-item">
+        <span class="ststus-label">UV-INDEX:</span>
+    </div>
+  </head>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'StatusHeader',
+});
+</script>
+
+<style scoped>
+    h1{
+        display: flex;
+    }
+</style>
