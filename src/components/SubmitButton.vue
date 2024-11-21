a5b65eead9cef95985854b4398dd43dadea4a9eb
Henri Dohmen <henri.dohmen@stud.tu-darmstadt.de>
2024-11-21 17:16:48 +0100

    Working prototype, some editor condiguration

--- src/components/SubmitButton.vue
+++ src/components/SubmitButton.vue
@@ -1,145 +1,140 @@
 <template>
-    <div>
-        <!-- Datei-Auswahl-Button -->
-        <label class="add-button" @click="triggerFileSelection" for="file-input">+</label>
-        <input type="file" ref="fileInput" 
-            id="file-input"
-            @change="onFileSelected" 
-            style="display: none;" 
-            accept=".json" />
+  <div>
+    <!-- Datei-Auswahl-Button -->
+    <label class="add-button" @click="triggerFileSelection" for="file-input">+</label>
+    <input type="file" ref="fileInput" id="file-input" @change="onFileSelected" style="display: none;" accept=".json" />
+  </div>
+  <div class="overlay-on-select" v-if="selectedFile">
+    <div class="overlay-container">
+      <h2>Selected File:</h2>
+      <h4>{{ selectedFile.name }}</h4>
+      <div class="form-buttons">
+        <!-- Absende-Button -->
+        <button v-if="selectedFile" @click="submitFile">Submit</button>
+        <button @click="closeForm">Close</button>
+      </div>
     </div>
-    <div class="overlay-on-select" v-if="selectedFile">
-        <div class="overlay-container"> 
-            <h2>Selected File:</h2>
-            <h4>{{selectedFile.name }}</h4>
-            <div class="form-buttons">
-                <!-- Absende-Button -->
-                <button v-if="selectedFile" @click="submitFile">Submit</button>
-                <button @click="closeForm">Close</button>
-            </div>
-        </div>
-    </div>
+  </div>
 </template>
-  
+
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-  
+import axios from 'axios';
+
 export default defineComponent({
-    name: 'SubmitButton',
-    setup() {
-        const baseURL = process.env.VUE_APP_API_URL; //Access .env variable 
-        const selectedFile = ref<File | null>(null);
-        const fileInput = ref<HTMLInputElement | null>(null);
-        
-        const closeForm = () => {
-            selectedFile.value = null; // Setze die Datei auf null, um das Overlay auszublenden
-        };
+  name: 'SubmitButton',
+  setup() {
+    const selectedFile = ref<File | null>(null);
+    const fileInput = ref<HTMLInputElement | null>(null);
 
-        //öffnet Dateiauswahlfenster
-        const triggerFileSelection = () => {
-            fileInput.value?.click(); //Simuliert Mausklick auf input-Feld wenn fileInput.value nicht NULL oder undefined
-        };
-
-        // Datei wird ausgewählt
-        const onFileSelected = (event: Event) => {
-            const target = event.target as HTMLInputElement;
-            const [file] = target.files || []; // Array Destructuring, weil der Linter sonst meckert
-            if (!file) {
-                alert('No file selected!');
-                return;
-            }
-            //Nur .json zulassen (höherer Schutz als rein im input tag)
-            if (file.type !== 'application/json' && !file.name.endsWith('.json')) {
-                alert('Unvalid file selected. Only .json files are allowed');
-                target.value = '';
-                return;
-            }
-            selectedFile.value = file;
-        };
-
-        // Datei absenden beispielhaft 
-        // Funktioniert natürlich noch nicht
-        /* TODO:
-            - Axios statt fetch nutzen 
-        */ 
-        const submitFile = () => {
-            if (selectedFile.value) {
-                const formData = new FormData();
-                formData.append('file', selectedFile.value);
-                // Beispiel: Datei an Backend senden
-                fetch('${baseURL}/upload', { //Beispiel pfad
-                    method: 'POST',
-                    body: formData,
-                })
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log('Upload erfolgreich:', data);
-                    closeForm(); //Overlay schließen nach erfolgreichem submit
-                 })
-                .catch((error) => {
-                    console.error('Fehler beim Hochladen:', error);
-                });
-            }
-       };
-    return {
-        selectedFile,
-        triggerFileSelection,
-        onFileSelected,
-        submitFile,
-        closeForm,
+    const closeForm = () => {
+      selectedFile.value = null; // Setze die Datei auf null, um das Overlay auszublenden
     };
-   },
+
+    // öffnet Dateiauswahlfenster
+    const triggerFileSelection = () => {
+      fileInput.value?.click();
+    };
+
+    // Datei wird ausgewählt
+    const onFileSelected = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      const [file] = target.files as FileList;
+      if (!file) {
+        alert('No file selected!');
+        return;
+      }
+      selectedFile.value = file;
+    };
+
+    // Datei absenden beispielhaft
+    // Funktioniert natürlich noch nicht
+    /* TODO:
+        - Axios statt fetch nutzen
+    */
+    const submitFile = () => {
+      if (!selectedFile.value) {
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('file', selectedFile.value);
+
+      axios.post('upload_file', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      }).then((response) => {
+        console.log(`Upload erfolgreich ${response}`);
+        closeForm();
+      }).catch((err) => {
+        console.log(`Fehler beim hochladen: ${err}`)
+      })
+    };
+    return {
+      selectedFile,
+      triggerFileSelection,
+      onFileSelected,
+      submitFile,
+      closeForm,
+    };
+  },
 });
 </script>
-  
+
 <style scoped>
 button {
-    padding: 10px 20px;
-    margin: 10px 0;
-    background-color: #ffffff;
-    color: rgb(0, 0, 0);
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  padding: 10px 20px;
+  margin: 10px 0;
+  background-color: #ffffff;
+  color: rgb(0, 0, 0);
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 }
-.add-button{
-    font-size: 50px;
+
+.add-button {
+  font-size: 50px;
 }
-.add-button:hover::after{
-    content: "Upload .json file";
-    position:absolute;
-    bottom: -20px;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #919191;
-    color: #fff;
-    padding: 5px 15px;
-    font-size: 10px;
-    border-radius: 4px;
-    white-space: nowrap;
-    z-index: 10;
+
+.add-button:hover::after {
+  content: "Upload .json file";
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #919191;
+  color: #fff;
+  padding: 5px 15px;
+  font-size: 10px;
+  border-radius: 4px;
+  white-space: nowrap;
+  z-index: 10;
 }
+
 .overlay-on-select {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5); /* Leicht verblasster Hintergrund */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 999; /* Überlagert den Inhalt */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  /* Leicht verblasster Hintergrund */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+  /* Überlagert den Inhalt */
 }
+
 /* Formular-Container */
 .overlay-container {
-    background-color: white;
-    padding: 20px;
-    padding-left: 10px;
-    padding-right: 30px;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    width: 300px;
+  background-color: white;
+  padding: 20px;
+  padding-left: 10px;
+  padding-right: 30px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 300px;
 }
 </style>
- 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import axios from 'axios';
 
+// not sure if this is the right way to do it...
+if(process.env.VUE_APP_PRCONTROL_API_URL == undefined) {
+  alert("VUE_APP_PRCONTROL_API_URL not set!");
+}
+axios.defaults.baseURL = process.env.VUE_APP_PRCONTROL_API_URL;
 createApp(App).mount('#app');
