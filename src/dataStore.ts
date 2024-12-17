2e87dd0fa18c98c764dfc6b07f879a1b2bee227c
Maurice Duchmann <maurice.duchmann@stud.tu-darmstadt.de>
2024-12-17 21:22:23 +0100

    extends ReactorBoxState and PowerBoxState types with new fields

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -2,17 +2,16 @@ import { reactive } from "vue";
 import webSocket from './webSocket';
 
 type ReactorBoxState = {
-  thermocouple_temp: number
-  ambient_light: number
+  thermocouble_temp: number
+  ambient_light:  number
   ambient_temperature: number
-  lane_ir_temp1: number
-  lane_ir_temp2:number
-  lane_ir_temp3: number
+  lane_1_ir_temp: number
+  lane_2_ir_temp: number
+  lane_3_ir_temp: number
   uv_index: number
-
-  lane_sample_taken1: boolean
-  lane_sample_taken2: boolean
-  lane_sample_taken3: boolean
+  lane_1_sample_taken: boolean
+  lane_2_sample_taken: boolean
+  lane_3_sample_taken: boolean
   maintenance_mode: boolean
   photobox_cable_control: boolean
 }
@@ -21,46 +20,80 @@ type PowerBoxState = {
   abmient_temperature: number
   voltage_total: number
   current_total: number
-  voltage_lane1: number
-  voltage_lane2: number
-  voltage_lane3: number
-  current_lane1: number
-  current_lane2: number
-  current_lane3: number
+  voltage_lane_1_front: number
+  voltage_lane_1_back: number
+  voltage_lane_2_front: number
+  voltage_lane_2_back: number
+  voltage_lane_3_front: number
+  voltage_lane_3_back: number
+  current_lane_1_front: number
+  current_lane_1_back: number
+  current_lane_2_front: number
+  current_lane_2_back: number
+  current_lane_3_front: number
+  current_lane_3_back: number
+
+  powerbox_closed: boolean
+  reactorbox_closed: boolean
+  led_installed_lane_1_front_and_vial: boolean
+  led_installed_lane_1_back: boolean
+  led_installed_lane_2_front_and_vial: boolean
+  led_installed_lane_2_back: boolean
+  led_installed_lane_3_front_and_vial: boolean
+  led_installed_lane_3_back: boolean
+  water_detected: boolean
 }
 
 type ReactorState = {
   reactor_box: ReactorBoxState;
   power_box: PowerBoxState;
 }
-
+//all set to 0 and false for demonstration purpose
 export const pcrData = reactive({
   allData: {
     reactor_box: {
-        thermocouple_temp: 0,
-        ambient_light: 0,
-        ambient_temperature: 0,
-        lane_ir_temp1: 0,
-        lane_ir_temp2: 0,
-        lane_ir_temp3: 0,
-        uv_index: 0,
-        lane_sample_taken1: false,
-        lane_sample_taken2: false,
-        lane_sample_taken3: false,
-        maintenance_mode: false,
-        photobox_cable_control: false,
+      thermocouble_temp: 0,
+      ambient_light:  0,
+      ambient_temperature: 0,
+      lane_1_ir_temp: 0,
+      lane_2_ir_temp: 0,
+      lane_3_ir_temp: 0,
+      uv_index: 0,
+      lane_1_sample_taken: false,
+      lane_2_sample_taken: false,
+      lane_3_sample_taken: false,
+      maintenance_mode: false,
+      photobox_cable_control: false,
     },
+
     power_box: {
-        abmient_temperature: 0,
-        voltage_total: 0,
-        current_total: 0,
-        voltage_lane1: 0,
-        voltage_lane2: 0,
-        voltage_lane3: 0,
-        current_lane1: 0,
-        current_lane2: 0,
-        current_lane3: 0,
+      abmient_temperature: 0,
+      voltage_total: 0,
+      current_total: 0,
+      voltage_lane_1_front: 0,
+      voltage_lane_1_back: 0,
+      voltage_lane_2_front: 0,
+      voltage_lane_2_back: 0,
+      voltage_lane_3_front: 0,
+      voltage_lane_3_back: 0,
+      current_lane_1_front: 0,
+      current_lane_1_back: 0,
+      current_lane_2_front: 0,
+      current_lane_2_back: 0,
+      current_lane_3_front: 0,
+      current_lane_3_back: 0,
+
+      powerbox_closed: false,
+      reactorbox_closed: false,
+      led_installed_lane_1_front_and_vial: false,
+      led_installed_lane_1_back: false,
+      led_installed_lane_2_front_and_vial: false,
+      led_installed_lane_2_back: false,
+      led_installed_lane_3_front_and_vial: false,
+      led_installed_lane_3_back: false,
+      water_detected: false,
     }
+
   } as ReactorState,
 
 
